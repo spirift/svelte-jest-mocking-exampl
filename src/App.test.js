695caec2c0a2f373bec0a0a488d81e87d72feb2e
spirift <drift.ra@gmail.com>
2020-04-30 23:34:15 +0100

    Remove unused imports

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,5 @@
 import App from './App.svelte'
 import { render } from '@testing-library/svelte'
-import MockSvelteSelect from './MockSvelteSelect.svelte'
-import MockSelect from 'svelte-select'
 
 test('it works', () => {
   const { getByText } = render(App)
